6d91828a1b336cff202aa4cd250ddfd0d4ec265f
lunaksj <lunaksj@naver.com>
2022-02-08 01:53:17 +0900

    주석을 지우고 ThreadID를 찍어봄

--- CsharpStudy/Program.cs
+++ CsharpStudy/Program.cs
@@ -9,7 +9,7 @@ namespace CsharpStudy
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("ThreadProc: {0}", i);
+                Console.WriteLine($"ThreadProc: {i}, TID:{Thread.CurrentThread.ManagedThreadId}");
                 // Yield the rest of the time slice.
                 Thread.Sleep(0);
             }
@@ -18,20 +18,13 @@ namespace CsharpStudy
         static void Main(string[] args)
         {
             Console.WriteLine("Main thread: Start a second thread.");
-            // The constructor for the Thread class requires a ThreadStart
-            // delegate that represents the method to be executed on the
-            // thread.  C# simplifies the creation of this delegate.
             Thread t = new Thread(new ThreadStart(ThreadProc));
-
-            // Start ThreadProc.  Note that on a uniprocessor, the new
-            // thread does not get any processor time until the main thread
-            // is preempted or yields.  Uncomment the Thread.Sleep that
-            // follows t.Start() to see the difference.
+            
             t.Start();
             //Thread.Sleep(0);
 
             for (int i = 0; i < 4; i++) {
-                Console.WriteLine("Main thread: Do some work.");
+                Console.WriteLine($"Main thread: Do some work.{Thread.CurrentThread.ManagedThreadId}");
                 Thread.Sleep(0);
             }
 
